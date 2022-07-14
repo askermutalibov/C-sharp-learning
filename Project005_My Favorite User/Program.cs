91401d58fb56b30f601461b755555d88423bde71
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-14 15:22:54 +0300

    5 project with warm greetings to fav user has been added

---
+++ Project005_My Favorite User/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "владимир ильич")
+{
+    Console.WriteLine("Здравия желаю, Владимир Ильич!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
