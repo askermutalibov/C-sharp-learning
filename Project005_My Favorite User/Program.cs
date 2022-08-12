cffca7d26e8c2eaffece114ef51fb6ce69fe478b
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-12 17:49:05 +0300

    005 proj (My Favorite User) has been copied from GitHub

--- Project005_My Favorite User/Program.cs
+++ Project005_My Favorite User/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// нужно поприветствовать одного из наиболее лояльных к сервису пользователей
+
+Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if (username?.ToLower() == "владимир ильич") // библиотека "ToLower" нужна для игнорирования регистра букв, которые вводит пользователь: если нужен пользователь с никнеймом MathDoesNotNeededForProgrammingBecauseBlahBlahBlah, то система распознает его так: mathdoesnotneededforprogrammingbecauseblahblahblah, так: MATHDOESNOTNEEDEDFORPROGRAMMINGBECAUSEBLAHBLAHBLAH и даже так: mAtHdOesNotnEedeDforProGrAMmingbecauseblahblahblah
+{
+    Console.WriteLine("Здравия желаю, Владимир Ильич!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
