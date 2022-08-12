78c6bdbf01cb42fd98c9d6c41757a6d5c8e598f3
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-12 18:19:02 +0300

    0010 proj (MethodArray) has been copied from GitHub

--- Project0010_MethodArray/Program.cs
+++ Project0010_MethodArray/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Теперь напишем код для поиска элемента в массиве из n элементов, который равен find. Это продвинутая версия 9 проекта.
+
+int[] array = { 1, 12, 31, 4, 15, 16, 17, 18 };
+
+int n = array.Length; // возвращает длину (количество элементов) массива
+int find = 18; // ищем элемент с 7 индексом, равный 18
+int index = 0; // стартовое значение индекса
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index); // лайтовая версия - как только алгоритм находит нужный элемент, он останавливается (ниже команда break)
+        break;
+    }
+    index++;
+}
