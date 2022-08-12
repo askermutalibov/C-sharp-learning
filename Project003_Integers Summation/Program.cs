9c07da2b2add170e8c677c6b6a418695d7d3b13c
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-12 17:19:04 +0300

    003 proj (Integers Summation) has been copied from GitHub

--- Project003_Integers Summation/Program.cs
+++ Project003_Integers Summation/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// нужно вывести сумму 2 случайных цифр
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9  это инициализатор массива с псевдослучайными числами, заданный в пределах от 1 включительно до 9 включительно
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
