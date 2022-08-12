2c376ea558808aa4bc9f5f1aac29f546ec858217
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-12 17:42:11 +0300

    004 proj (Integers Summation) has been copied from GitHub

--- Project004_Integers Divide/Program.cs
+++ Project004_Integers Divide/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// нужно вывести частное от деления 2 заданных чисел
+
+double NumberA = 19;
+double NumberB = 5;
+Console.WriteLine(NumberA / NumberB);
